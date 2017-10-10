8969449d952d404200c28f5d70ab14f77466a7fb
Tomas <tomasborro@yahoo.es>
2017-10-10 20:47:54 -0300

    Creacion de nuevo nivel

--- Kill Them All/Assets/Scripts/LevelSelector.cs
+++ Kill Them All/Assets/Scripts/LevelSelector.cs
@@ -6,12 +6,14 @@ public class LevelSelector : MonoBehaviour {
 
 	public Button level1;
 	public Button level2;
+    public Button level3;
 	
 	// Update is called once per frame
 	void Update () {
 		level1.onClick.AddListener (FirstLevel);
 		level2.onClick.AddListener (SecondLevel);
-	}
+        level3.onClick.AddListener(ThirdLevel);
+    }
 
 	void FirstLevel(){ 
 		SceneManager.LoadScene ("Scene");
@@ -19,4 +21,8 @@ public class LevelSelector : MonoBehaviour {
 	void SecondLevel(){ 
 		SceneManager.LoadScene ("Scene2"); 
 	}
+    void ThirdLevel()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
 }
