d7955d27dd648fdeeec0ec8eb15c3fce1619ebfd
ssforzini <sebasforzini@hotmail.com>
2017-10-31 01:31:08 -0300

    Se hicieron modificaciones en el script de la explosion de un arma. falta mejorarlo

--- Kill Them All/Assets/Scripts/FloorExplosion.cs
+++ Kill Them All/Assets/Scripts/FloorExplosion.cs
@@ -6,6 +6,10 @@ public class FloorExplosion : MonoBehaviour {
 
     private Rigidbody rb;
 
+	private float power = 10.0f;
+	private float radius = 5.0f;
+	private float upForce = 1.0f;
+
 	// Use this for initialization
 	void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
@@ -18,9 +22,19 @@ public class FloorExplosion : MonoBehaviour {
 
     void OnTriggerEnter(Collider col) {
         if (col.gameObject.tag == "Enemies") {
-            col.GetComponent<Rigidbody>().AddExplosionForce(15000f, transform.position, 15000f);
-            //Destroy(gameObject);
+			Invoke ("Detonate",1);
         }
-
     }
+
+	void Detonate(){
+		Vector3 explosionPosition = gameObject.transform.position;
+		Collider[] colliders = Physics.OverlapSphere (explosionPosition,radius);
+		foreach(Collider hit in colliders){
+			Debug.Log (hit.gameObject.name);
+			Rigidbody rig = hit.GetComponent<Rigidbody> ();
+			if(rig != null){
+				rig.AddExplosionForce (power,explosionPosition,radius,upForce,ForceMode.Impulse);
+			}
+		}
+	}
 }
